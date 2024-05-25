16f101d47c0d3b3c8a7f0dcd5c0939f7f438e20e
MEisSCAMMER <ishaan.ramesh@gmail.com>
2024-05-25 11:25:43 -0400

    combine __init__()s
    apparently you can't overload constructors in Python, so I juggled some stuff around until I had one that did the job of both. also, I did some reformatting

--- QR Backend/BatteryDB.py
+++ QR Backend/BatteryDB.py
@@ -2,21 +2,22 @@ import sqlite3
 
 
 class Battery:
-    def __init__(self, _id, name, percentage, num_cycles):
-        self._id = _id
-        self.name = name
-        self.percentage = percentage
-        self.num_cycles = num_cycles
-
-    def __init__(self, sql_input):
-        self._id = sql_input[0]
-        self.name = sql_input[1]
-        self.percentage = sql_input[2]
-        self.num_cycles = sql_input[3]
+    def __init__(self, sql_input, _id: int = 0, name: str = "None", percentage: int = 0, num_cycles: int = 0):
+        if sql_input is not None:
+            self._id = sql_input[0]
+            self.name = sql_input[1]
+            self.percentage = sql_input[2]
+            self.num_cycles = sql_input[3]
+        else:
+            self._id = _id
+            self.name = name
+            self.percentage = percentage
+            self.num_cycles = num_cycles
 
     def get_id(self):
         return self._id
-    
+
+
 class BatteryData:
     def __init__(self, battery: Battery, is_charged: bool, percentage: int, voltage: float, load_voltage: float):
         self.battery = battery
@@ -26,14 +27,25 @@ class BatteryData:
         self.load_voltage = load_voltage
         self.battery_id = battery.get_id()
 
+
 class BatteryDB:
     def __init__(self, db_file="batteries.db"):
         self.db_file = db_file
         conn = sqlite3.Connection(self.db_file)
         cursor = conn.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS batteries (id INTEGER PRIMARY KEY, name STRING, percentage INTEGER NOT NULL, num_cycles INTEGER NOT NULL)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS batteries_data (data_id INTEGER PRIMARY KEY, battery_id INTEGER NOT NULL, cycle_num INTEGER NOT NULL, is_charged BOOLEAN NOT NULL, percentage INTEGER NOT NULL, voltage FLOAT NOT NULL, load_voltage FLOAT NOT NULL)")
-        # should percentage be an int or a float?
+        cursor.execute("CREATE TABLE IF NOT EXISTS batteries (id INTEGER PRIMARY KEY, name STRING, "
+                       "percentage INTEGER NOT NULL, num_cycles INTEGER NOT NULL)")
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS batteries_data (
+                data_id INTEGER PRIMARY KEY, 
+                battery_id INTEGER NOT NULL, 
+                cycle_num INTEGER NOT NULL, 
+                is_charged BOOLEAN NOT NULL, 
+                percentage INTEGER NOT NULL, 
+                voltage FLOAT NOT NULL, 
+                load_voltage FLOAT NOT NULL
+            )""")
+
         self.batteries = cursor.execute("SELECT * FROM batteries").fetchall()
         conn.commit()
         conn.close()
@@ -52,7 +64,8 @@ class BatteryDB:
         battery_id = battery_data.battery_id
         conn = sqlite3.Connection("batteries.db")
         cursor = conn.cursor()
-        current_battery = Battery(cursor.execute("SELECT * FROM batteries WHERE id="+ str(battery_id) +"").fetchall()[0])
+        current_battery = Battery(
+            cursor.execute("SELECT * FROM batteries WHERE id=" + str(battery_id) + "").fetchall()[0])
         print(current_battery)
         #do stuff here
         cursor.execute("UPDATE batteries (id, name, percentage, num_cycles) VALUES " +
@@ -60,6 +73,6 @@ class BatteryDB:
         self.batteries = [Battery(t) for t in cursor.execute("SELECT * FROM batteries").fetchall()]
         conn.commit()
         conn.close()
-    
+
     def get_battery(self, _id: int):
         return self.batteries[_id]
